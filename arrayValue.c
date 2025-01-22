8bf0ae1fd48fc1e9ecfc423ffce9520090252d86
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2025-01-22 14:26:30 +0530

    Update arrayValue.c

--- arrayValue.c
+++ arrayValue.c
@@ -3,3 +3,10 @@ int getValueFromArray(int *array, int len, int index) {
 // Return the element from the array at the specified index
   return array[index];
 }
+int main() {
+  int array[] = {10, 20, 30, 40, 50};
+  int len = sizeof(array) / sizeof(array[0]);
+  int index = 2;
+  int value = getValueFromArray(array, len, index);
+  return 0;
+}
