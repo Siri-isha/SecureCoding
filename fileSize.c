f2675e27732c1178cad32da628c602aeed6ac9a9
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2025-01-24 17:03:31 +0530

    Update fileSize.c

--- fileSize.c
+++ fileSize.c
@@ -11,7 +11,25 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
+
+ //Give an upper bound to the input
+ if(strlen(argv[1] > 250)
+   fprintf("The input file name is invalid as it is longer than 250 characters");
+ 
+ char str[250];
+
+ //check if the input string contains special characters like | or &
+  str = argv[1];
+ int i = 0;
+ while (str[i] != '\0') {
+   if(str[i] == '|' || str[i] == '&') {
+            fprintf("Character at index %d: %c\n", i, str[i]);
+    return -1;
+   }
+  i++;
+ }
     char cmd[BUFSIZE] = "wc -c < ";
+    
     strcat(cmd, argv[1]);
     system(cmd);
 }
