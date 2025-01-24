cb2c2d874f3e1db55d2c8bbb0547917f54cc2d91
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2025-01-24 17:08:02 +0530

    Corrected code

--- fileSize.c
+++ fileSize.c
@@ -1,9 +1,9 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -12,24 +12,29 @@ int main(int argc, char** argv) {
         return -1;
     }
 
- //Give an upper bound to the input
- if(strlen(argv[1] > 250)
-   fprintf("The input file name is invalid as it is longer than 250 characters");
- 
- char str[250];
+    // Give an upper bound to the input
+    if (strlen(argv[1]) > 250) {
+        fprintf(stderr, "The input file name is invalid as it is longer than 250 characters.\n");
+        return -1;
+    }
 
- //check if the input string contains special characters like | or &
-  str = argv[1];
- int i = 0;
- while (str[i] != '\0') {
-   if(str[i] == '|' || str[i] == '&') {
-            fprintf("Character at index %d: %c\n", i, str[i]);
-    return -1;
-   }
-  i++;
- }
+    char str[250];
+    strncpy(str, argv[1], sizeof(str) - 1);
+    str[sizeof(str) - 1] = '\0';
+
+    // Check if the input string contains special characters like | or &
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] == '|' || str[i] == '&') {
+            fprintf(stderr, "Character at index %d: %c\n", i, str[i]);
+            return -1;
+        }
+        i++;
+    }
+
     char cmd[BUFSIZE] = "wc -c < ";
-    
     strcat(cmd, argv[1]);
     system(cmd);
+
+    return 0;
 }
