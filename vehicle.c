e0132da42e967b4fc440b746214c42973a9d7cb0
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2025-01-30 12:41:55 +0530

    Update vehicle.c

--- vehicle.c
+++ vehicle.c
@@ -3,6 +3,6 @@
     
 int main(int argc, char **argv) {
     char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
+    int index = atoi(argv[4]);
     printf("You selected %s\n", items[index]);
 }
