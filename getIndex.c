8df2158b6f34d69c6f59f473fa4ae2f035e51f59
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2025-01-30 13:14:26 +0530

    Update getIndex.c

--- getIndex.c
+++ getIndex.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
 int getValueFromArray(int *array, int len, int index) {
-return array[index];
+    return array[index];
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int index = 2;
+
+    int value = getValueFromArray(arr, len, index);
+
+    return 0;
 }
